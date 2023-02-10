0d61baad7ed370dff4f1849a179ef69acd6c391b
yixin0829 <yixin0829@outlook.com>
2023-02-09 23:07:47 -0500

    added react widget for practicing

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,16 @@ import {
 } from '@jupyterlab/apputils';
 
 import { Widget } from '@lumino/widgets';
+import { CounterWidget } from './react_widget';
+import { reactIcon } from '@jupyterlab/ui-components';
+
+/**
+ * The command IDs used by the react-widget plugin.
+ */
+namespace CommandIDs {
+  export const react_widget = 'create-react-widget';
+  export const apod = 'apod:open';
+}
 
 // define the type that was introduced in the newWidget() function
 interface APODResponse {
@@ -103,8 +113,8 @@ function activate(app: JupyterFrontEnd, palette: ICommandPalette, restorer: ILay
   let widget: MainAreaWidget<APODWidget>;
 
   // Add an application command
-  const command: string = 'apod:open';
-  app.commands.addCommand(command, {
+  let command_1:string = CommandIDs.apod
+  app.commands.addCommand(command_1, {
     label: 'Random Astronomy Picture',
     execute: () => {
       if (!widget || widget.isDisposed) {
@@ -130,25 +140,67 @@ function activate(app: JupyterFrontEnd, palette: ICommandPalette, restorer: ILay
   });
 
   // Add the command to the palette.
-  palette.addItem({ command, category: 'Tutorial' });
+  //* Note: if command string is the same as the key "command" then you can just write {command , category: ...}
+  palette.addItem({ command: command_1, category: 'Tutorial' }); 
 
   // Track and restore the widget state
   let tracker = new WidgetTracker<MainAreaWidget<APODWidget>>({
     namespace: 'apod'
   });
+
+  // Since the plugin token is declared as optional so restorer can be ILayoutRestorer | null
   if (restorer) {
     restorer.restore(tracker, {
-      command,
+      command: command_1, //* Note: if command string is the same as the key "command" then you can just write command (value only)
       name: () => 'apod'
     });
   }
+
+  let command_2:string = CommandIDs.react_widget
+  app.commands.addCommand(command_2, {
+    caption: 'Create a new React Widget',
+    label: 'React Counter Widget',
+    execute: () => {
+      const content = new CounterWidget();
+      const widget = new MainAreaWidget<CounterWidget>({ content });
+      widget.title.label = 'React Widget';
+      widget.title.icon = reactIcon;
+
+      if (!tracker_counter.has(widget)) {
+        // Track the state of the widget for later restoration
+        tracker_counter.add(widget);
+      }
+
+      if (!widget.isAttached) {
+        // Attach the widget to the main work area if it's not there
+        app.shell.add(widget, 'main');
+      }
+    },
+  });
+
+  // Add the command to the palette
+  palette.addItem({ command: command_2, category: 'Tutorial' });
+
+  // Track and restore the widget state
+  let tracker_counter = new WidgetTracker<MainAreaWidget<CounterWidget>>({
+    namespace: 'react_counter'
+  });
+
+  // Since the plugin token is declared as optional so restorer can be ILayoutRestorer | null
+  if (restorer) {
+    restorer.restore(tracker_counter, {
+      command: command_2,
+      name: () => 'react_counter'
+    });
+  }
+
 }
 
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab_apod',
   autoStart: true,
-  requires: [ICommandPalette],
-  optional: [ILayoutRestorer],
+  requires: [ICommandPalette], // lists of tokens corresponding to services other plugins provide.
+  optional: [ILayoutRestorer], // lists of tokens corresponding to services other plugins provide.
   activate: activate
 };
 
